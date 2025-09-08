bb45ea7ee1f0ac1c2bd45aeabf694c412ab7bd00
Sanyam Pandey <sanyam071104@gmail.com>
2025-09-08 22:26:15 +0530

    done

--- src/utils/peerConfiguration.js
+++ src/utils/peerConfiguration.js
@@ -1,14 +1,18 @@
-
 // utils/peerConfiguration.js
 const peerConfiguration = {
   iceServers: [
-    { urls: [
-      'stun:stun.l.google.com:19302',
-      'stun:stun1.l.google.com:19302'
-    ]}
+    {
+      urls: [
+        "stun:stun.l.google.com:19302",
+        "stun:stun1.l.google.com:19302",
+      ],
+    },
+    {
+      urls: "turn:relay1.expressturn.com:3480",
+      username: "000000002072709872",
+      credential: "RcNGsN/ChbwjcrwJKP71rlZoaHE=",
+    },
   ],
-  bundlePolicy: "max-bundle",
-  rtcpMuxPolicy: "require"
 };
 
 export default peerConfiguration;
