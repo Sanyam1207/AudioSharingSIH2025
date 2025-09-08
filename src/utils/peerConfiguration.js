ffea0aabe6c3fc5a7e5f862635301687f8f3ce12
Sanyam Pandey <sanyam071104@gmail.com>
2025-09-08 23:05:42 +0530

    Update peerConfiguration.js

--- src/utils/peerConfiguration.js
+++ src/utils/peerConfiguration.js
@@ -1,19 +1,32 @@
 // utils/peerConfiguration.js
 const peerConfiguration = {
   iceServers: [
-    { urls: "stun:stun.l.google.com:19302" },
     {
-      urls: [
-        "turn:relay1.expressturn.com:3478",
-        "turn:relay1.expressturn.com:3479",
-        "turn:relay1.expressturn.com:3480",
-        "turn:relay1.expressturn.com:5349",
-      ],
-      username: "000000002072709872",
-      credential: "RcNGsN/ChbwjcrwJKP71rlZoaHE=",
+      urls: "stun:stun.relay.metered.ca:80"
     },
-  ],
+    {
+      urls: "turn:asia.relay.metered.ca:80",
+      username: "72f03df1b6a58f38d7fd81ab",
+      credential: "guctVzh/8qDU4KU0"
+    },
+    {
+      urls: "turn:asia.relay.metered.ca:80?transport=tcp",
+      username: "72f03df1b6a58f38d7fd81ab",
+      credential: "guctVzh/8qDU4KU0"
+    },
+    {
+      urls: "turn:asia.relay.metered.ca:443",
+      username: "72f03df1b6a58f38d7fd81ab",
+      credential: "guctVzh/8qDU4KU0"
+    },
+    {
+      urls: "turns:asia.relay.metered.ca:443?transport=tcp",
+      username: "72f03df1b6a58f38d7fd81ab",
+      credential: "guctVzh/8qDU4KU0"
+    }
+  ]
 };
+
 
 export default peerConfiguration;
 
